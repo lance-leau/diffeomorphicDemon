6fd8ff2bdfd255261db1c40932458654c2bff66b
lance-leau <lancelot.dou7@gmail.com>
2025-10-07 15:51:18 +0200

    feat: added the matlab entry point

---
+++ diffeoDemon.c
@@ -0,0 +1,31 @@
+#include "demon.h"
+#include "mex.h"
+#include "tools.h"
+
+void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
+{
+    if (nrhs < 2)
+        mexErrMsgTxt("Usage: [warped, field] = diffeoDemon(fixed, moving)");
+    if (nlhs > 2)
+        mexErrMsgTxt("Too many output arguments.");
+
+    Image* fixed = parseMatlabToImage(prhs[0]);
+    Image* moving = parseMatlabToImage(prhs[1]);
+
+    normalizeImage(fixed);
+    normalizeImage(moving);
+
+    DispField* df = initDispField(fixed->width, fixed->height);
+
+    // demonsRegistration(fixed, moving, df, 16, 4, 20, 1.0f);
+
+    // Image* warped = warpImage(moving, df);
+
+    // TODO free the everything
+    // TODO parse fixed and moving to matlab compatible structs and return
+
+    plhs[0] = parseDispFieldToMatlab(df);
+    plhs[1] = parseImageToMatlab(moving);
+
+    return;
+}
